79f57878e0b39d72dc597e028c99ec8d7eda35f9
Florian SALBER <florian.salber@gmail.com>
2017-11-29 09:35:51 +0100

    [UPDATE] Add ADMINER into tools section, edit style and remove MAMP from page Title

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$excludeDir = ['.', '..', 'ressources'];
+$excludeDir = ['.', '..', 'ressources', 'adminer'];
 $dirList = array_diff(scandir('.'), $excludeDir);
 foreach ($dirList as $element){
     if(!is_dir($element)){
@@ -18,7 +18,7 @@ foreach ($dirList as $element){
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <title>Accueil - MAMP <?= $_SERVER['HTTP_HOST']; ?></title>
+    <title>Accueil - <?= $_SERVER['HTTP_HOST']; ?></title>
 </head>
 <body>
 <div>
@@ -31,16 +31,35 @@ foreach ($dirList as $element){
             <?php
             foreach ($dirList as $element){
                 ?>
-                <div class="col s6 element" id="<?= $element ?>">
-                    <a href="<?= $element ?>">
-                        <ul class="collection">
-                            <li class="collection-item avatar valign-wrapper">
-                                <i class="material-icons circle">folder</i>
-                                <span class="title"><?= $element ?></span>
-                                <i class="material-icons secondary-content">open_in_new</i>
-                            </li>
-                        </ul>
-                    </a>
+                <div class="col s6 m6">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="<?= $element ?>/example.jpg">
+                            <span class="card-title"><?= $element ?></span>
+                        </div>
+                        <div class="card-content">
+                            <p>
+                                <?php
+                                    $exclude = ['.', '..', '.DS_Store']; 
+                                    $subelements = array_diff(scandir($element), $exclude);   
+                                    $subfolders = 0;
+                                    $subfiles = 0;
+                                    foreach($subelements as $k => $v) {
+                                        if(is_dir($element.'/'.$v)) {
+                                            $subfolders++;
+                                        }
+                                        if(is_file($element.'/'.$v)) {
+                                            $subfiles++;
+                                        }
+                                    }                        
+                                    echo "$subfolders Dossiers / $subfiles Fichiers";
+                                ?>
+                            </p>
+                        </div>
+                        <div class="card-action">
+                            <a class="green-text" href="<?= $element ?>">Accéder au projet</a>
+                        </div>
+                    </div>
                 </div>
                 <?php
             }
@@ -64,6 +83,10 @@ foreach ($dirList as $element){
             <div class="col s12">
                 <input type="text" id="toSLUG" name="toSLUG" placeholder="Chaine de charactères à slugger">
                 <span class="toSLUG">Résultat : <b>AUCUNE CHAINE DE CHARACTERES A SLUGGER</b></span>
+            </div>
+            <p>&nbsp;</p>
+            <div class="col s12">
+                <a class="waves-effect waves-light btn" href="adminer"><i class="material-icons left">cloud</i>Base de données <small>(Adminer)</small></a>
             </div>
         </div>
     </div>
